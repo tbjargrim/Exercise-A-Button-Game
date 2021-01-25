078a78554aa7fd5cc62dea218ddc22d6eef7db84
Tillbet <55994723+Tillbet@users.noreply.github.com>
2021-01-25 20:49:57 +0100

    made the basics of all pages

--- scriptFour.js
+++ scriptFour.js
@@ -1,71 +1,37 @@
-const quiz = document.querySelector('#quiz');
-const results = document.querySelector('#results');
-const submitBtn = document.querySelector('#submit');
+const container= document.getElementById('container');
+const colors = ['#4DA859', '#389CBA', '#BA38B6', '#B22742','#FFBAA1'];
+const SQUARES= 500;
+const btn = document.querySelector('.buttonFour');
+const body = document.querySelector('.bodyFour');
+for (let i = 0; i < SQUARES; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
 
-const myQuestions =[
-    { 
-        question:"How many Earths could fit inside the sun?",
-        answers:{
-            a: '3',
-            b: '1,300',
-            c: '1.3 millions'
-        },
-        correctAnswer:'c'
-    },
-    {
-        question: "Which country consumes the most chocolate per capita?",
-        answers: {
-            a: 'Sweden',
-            b: 'Switzerland',
-            c: 'Australia'
-        },
-        correctAnswer: 'Switzerland'
-    },
-    {
-        question: "In which country was the largest-known Tyrannosaurus rex skeleton found?",
-        answers: {
-            a: 'Canada',
-            b: 'Brazil',
-            c: 'South Africa'
-        },
-        correctAnswer: 'Canada'
-    }
-]
-function buildQuiz() {
-    const output=[];
+        square.addEventListener('mouseover', () => setColor(square))
+    
+    square.addEventListener('mouseout', () => removeColor(square))
+   
+    container.appendChild(square)
+}
 
-    myQuestions.forEach(
-        (currentQuestion,questionNumber) => {
-            const answers= [];
+function setStyleButton(){
 
-            for(letter in currentQuestions.answers){
-                answers.push(
-                    `
-                    <label>
-                    <input typ="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>
-                    `
-                );
-            }
-            output.push(
-                `
-                <div class="question"> ${currentQuestion.question} </div>
-               
-                <div class="answers"> ${answers.join('')} </div>`
-            );
-        }
-    )
- }
-// join, joins everything together and puts it into a string
- quiz.innerHTML = output.join('');
+}
+btn.addEventListener('click',() => 
+ body.classList.add('endOfGame'));
 
 
-function showResults() { }
 
-// display quiz right away
-buildQuiz();
+function setColor(element){
+const color= getRandomColor()
+element.style.background=color;
+element.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`;
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+}
+function removeColor(element){
+element.style.background = `#1d1d1d`;
+element.style.boxShadow= `0 0 2px #000`;
+}
+function getRandomColor(){
+    return colors[Math.floor(Math.random()*colors.length)]
+}
